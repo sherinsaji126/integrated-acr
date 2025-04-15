675dcef6a58dc4c2684e11de58d7ccea6117ec82
sherinsaji126 <sherinsaji126@gmail.com>
2025-04-15 12:32:53 +0530

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "85b87ac7-2879-4c21-a8ad-a45ac94c5fff"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "mysherin-newrg"
+  location = "East US"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "myacrregistry123"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "myakscluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "myaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+    # Linux OS is used by default – no need to set os_type
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
