f6b742589af6725af23420a1ab7afc6608393c12
Sherin saji <146155179+sherinsaji126@users.noreply.github.com>
2025-04-16 13:24:10 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,79 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "85b87ac7-2879-4c21-a8ad-a45ac94c5fff"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+  required_version = ">=1.0.0"
 }
 
-resource "azurerm_resource_group" "rg" {
+provider "azurerm" {
+  features {}
+  resource_provider_registrations = "none"
+  subscription_id                 = "85b87ac7-2879-4c21-a8ad-a45ac94c5fff"
+  tenant_id                       = "4dd7a366-bffd-434d-ad00-bbbee4d2001f" 
+}
+
+resource "azurerm_resource_group" "main" {
   name     = "mysherin-newrg"
-  location = "East US2"
+  location = "eastus2"
 }
 
 resource "azurerm_container_registry" "acr" {
   name                = "mysherinacrregistry123"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
   sku                 = "Basic"
   admin_enabled       = true
+
+  tags = {
+    environment = "dev"
+  }
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "mysherinakscluster"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "myaks"
+  name                ="mysherinakscluster"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "${var.aks_name}-dns"
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 2
     vm_size    = "Standard_B2ms"
-    # Linux OS is used by default – no need to set os_type
   }
 
   identity {
     type = "SystemAssigned"
   }
 
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+
   tags = {
-    Environment = "Dev"
+    environment = "dev"
+  }
+
+  depends_on = [azurerm_container_registry.acr]
+}
+
+# Wait until AKS identity is fully available using a data source
+data "azurerm_kubernetes_cluster" "aks" {
+  name                = azurerm_kubernetes_cluster.aks.name
+  resource_group_name = azurerm_resource_group.main.name
+  depends_on          = [azurerm_kubernetes_cluster.aks]
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = data.azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+
+  # Avoids recreation if principal_id already has role
+  lifecycle {
+    ignore_changes = [principal_id]
   }
 }
