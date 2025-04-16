03330863bd855bb54e9f3716419d36a9e09a36f2
Sherin saji <146155179+sherinsaji126@users.noreply.github.com>
2025-04-16 09:50:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name       = "default"
-    node_count = 2
+    node_count = 1
     vm_size    = "Standard_B2s"
     # Linux OS is used by default – no need to set os_type
   }
