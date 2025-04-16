741a734656fcf35ca6fe6e8ea3e328ce56ced368
Sherin saji <146155179+sherinsaji126@users.noreply.github.com>
2025-04-16 13:38:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "mysherinacrregistry123"
+  name                = "mysherinsajiacrregistry123"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   sku                 = "Basic"
@@ -33,7 +33,7 @@ resource "azurerm_container_registry" "acr" {
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                ="mysherinakscluster"
+  name                ="mysherinsajiakscluster"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   dns_prefix          = "myaks"
