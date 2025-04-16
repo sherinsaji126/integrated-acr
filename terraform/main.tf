ac0218b5e10dad712486ec1163065ef524d3911b
Sherin saji <146155179+sherinsaji126@users.noreply.github.com>
2025-04-16 15:37:07 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,6 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.94.0"
-    }
-  }
-  required_version = ">=1.0.0"
-}
-
 provider "azurerm" {
   features {}
-  resource_provider_registrations = "none"
-  subscription_id                 = "85b87ac7-2879-4c21-a8ad-a45ac94c5fff"
-  tenant_id                       = "4dd7a366-bffd-434d-ad00-bbbee4d2001f"
+  subscription_id = "85b87ac7-2879-4c21-a8ad-a45ac94c5fff"
 }
 
 resource "azurerm_resource_group" "main" {
@@ -26,10 +14,6 @@ resource "azurerm_container_registry" "acr" {
   location            = azurerm_resource_group.main.location
   sku                 = "Basic"
   admin_enabled       = true
-
-  tags = {
-    environment = "dev"
-  }
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
@@ -41,44 +25,26 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_B2ms"
+    vm_size    =  "Standard_B2ms"
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  network_profile {
-    network_plugin    = "azure"
-    load_balancer_sku = "standard"
-  }
+  oidc_issuer_enabled       = true
+  workload_identity_enabled = false
 
-  tags = {
-    environment = "dev"
+  network_profile {
+    network_plugin = "azure"
   }
 
   depends_on = [azurerm_container_registry.acr]
 }
 
-# This ensures AKS identity is available after creation
-data "azurerm_kubernetes_cluster" "aks" {
-  name                = azurerm_kubernetes_cluster.aks.name
-  resource_group_name = azurerm_resource_group.main.name
-
-  depends_on = [azurerm_kubernetes_cluster.aks]
-}
-
 resource "azurerm_role_assignment" "acr_pull" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = data.azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-
-  depends_on = [
-    azurerm_kubernetes_cluster.aks,
-    azurerm_container_registry.acr
-  ]
-
-  lifecycle {
-    ignore_changes = [principal_id]
-  }
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id  # Indexing the list correctly
+  depends_on           = [azurerm_kubernetes_cluster.aks]
 }
